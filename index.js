a706383d22c15d7ca575b73d695450676d1bae86
Vennie Chang <venniechang@yahoo.com>
2018-03-13 10:23:25 -0700

    added map

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function generateCurrencyCard(data, symbol) {
 
 function showCurrencyData(data) { // want to display results to DOM
     let output = "";
-    console.log(data.rates);
+    //console.log(data.rates);
     for (let symbol in data.rates) {
     output += generateCurrencyCard(data, symbol);
     }
@@ -42,7 +42,7 @@ function storeCurrencyData(data) {
 function convertCurrencyAmount(data, multiplier) { //multiply each number in rates and store it in data in place of starting rate
     for (let symbol in data.rates) {
         data.rates[symbol] = data.rates[symbol] * multiplier;
-        console.log(symbol);
+        //console.log(symbol);
     }
     
     console.log(STORE.data);
@@ -86,9 +86,80 @@ function buildSymbolString(symbols){
     return symbolString;
 }
 
-//function roundCurrencyDigits(data){
-  //  console.log(data.rates);
-//}
 handleAmountChange();
 handleFormSubmit();
-//roundCurrencyDigits();
+
+function initMap() {
+    var uluru = {lat: 34.0522, lng: -118.2437};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru,
+          mapTypeId: 'roadmap'
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+        console.log(marker);
+
+    // Create the Search box and link it to the UI element    
+    var input = document.getElementById("bank-input");
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport
+    map.addListener("bounds_changed", function(){
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+
+    //Listen for the event fired when the user selects a prediction and retrieve more details for that place
+    searchBox.addListener("places_changed", function(){
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        //Clear out old markers
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+    
+        //For each place, get the icon, name and location
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+        //Create a marker for each place
+        markers.push(new google.maps.Marker({
+            map: map,
+            icon: icon,
+            tile: place.name,
+            position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+            //Only geocodes have viewport
+            bounds.union(place.geometry.viewport);
+        } 
+        
+        else {
+            bounds.extend(place.geometry.location);
+        }
+        });
+        map.fitBounds(bounds);
+   });
+}
